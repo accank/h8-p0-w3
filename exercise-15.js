f248ed7e97394651b9dc11c9ec4600d5cf52bd87
Muhammad <accank72@gmail.com>
2018-08-19 15:40:25 +0800

    revisi exercise-15.js

--- exercise-15.js
+++ exercise-15.js
@@ -1,15 +1,16 @@
 function groupAnimals(animals) {
   animals.sort();
   var klpAnimals = [];
+  var hurufAwal = animals[0][0];
   
   var kategori = [];
   for (var i = 0; i < animals.length; i++) {
-    if (animals[i][0] === animals[0][0]) {
+    if (animals[i][0] === hurufAwal) {
       kategori.push(animals[i]);
     } else {
       klpAnimals.push(kategori);
       kategori = [];
-      animals[0][0] = animals[i][0];
+      hurufAwal = animals[i][0];
       kategori.push(animals[i]);
     }
     if (i === animals.length-1) {
